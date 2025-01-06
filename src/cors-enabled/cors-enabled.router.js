7753cdacbee963e87f81f2d1f607b8619ccb2104
John <robnaratt34@gmail.com>
2025-01-05 22:05:55 -0700

    Allowed all CORS requests

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,21 +1,18 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-const cors = require("cors")
 
-const corsDelete = cors({methods: "DELETE"});
 
 router
   .route("/:corsId")
   .get(controller.read)
   .put(controller.update)
-  .delete(corsDelete, controller.delete)
-  .options(corsDelete)
+  .delete(controller.delete)
+  
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .all(cors())
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
